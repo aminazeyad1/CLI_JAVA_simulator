b846cea2d77e3d3f495eeaac62a8d3b7a4b3e586
Yaseen Emad <118124426+YaseenEmad@users.noreply.github.com>
2023-08-03 23:55:38 +0300

    Update Terminal.java

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,26 +1,42 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Terminal {
     Parser parser; //"Parser" class object
 
     //This method will choose the suitable command method to be called
     public void chooseCommandAction(){
+
         Terminal tm = new Terminal();
-        if (parser.getCommandName() == "cd"){tm.cd(parser.getArgs());}
         if (parser.getCommandName() == "date"){tm.date();}
+        if (parser.getCommandName() == "cd"){tm.cd(parser.getArgs());}
         // and so on
     }
-    public void clear(){}
+    public void clear(){
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
     public void cd(String[] args){}
     public void ls(){}
     public void pwd(){}
-    public void date() {}
+    public void date() {
+        Date currentDate = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("EET"));
+        String formattedDate = dateFormat.format(currentDate);
+        System.out.println(formattedDate);
+
+    }
     public void mv(String[] args) {}
     public void rm(String [] args){}
     public void mkdir (String[] args) {}
     public void rmdir (String[] args){}
-    public void cp (String[] args){}
+    public void cp (String[] args){} //cp file1.txt file2.txt
     public void more (String[] args){}
     public void cat (String[] args){}
-    public void exit (){}
+    public void exit (){
+        System.exit(0);
+    }
     public void help (){
         System.out.println(
                 "clear - Clear the console\n" +
