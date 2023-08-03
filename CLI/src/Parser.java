ec9eab11fd83bf347e3d6e2e8389e9ff26942236
Yaseen Emad <118124426+YaseenEmad@users.noreply.github.com>
2023-08-03 22:20:08 +0300

    Update Parser.java

--- CLI/src/Parser.java
+++ CLI/src/Parser.java
@@ -4,7 +4,9 @@ public class Parser {
 
     //This method will divide the input into commandName and args, where "input" is the string command entered by the user
     //It also checks validation of the input/number of args if valid --> return true, if not --> prints error message then return false
-    public boolean parse(String input){}
+    public boolean parse(String input){
+        
+    }
     public String getCommandName(){
         return commandName;
     }
